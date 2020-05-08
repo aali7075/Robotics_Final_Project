ddadc2b38fccb91b6588ea8c14a5598ea6ab6a5f
Ean Kramer <ean1001@gmail.com>
2020-05-07 18:22:34 -0600

    updated ball_detect

--- scripts/ball_detect.py
+++ scripts/ball_detect.py
@@ -5,10 +5,10 @@ from geometry_msgs.msg import Point
 import rospy
 import numpy as np
 
-l_b_1 = np.array([0, 200, 94])
-u_b_1 = np.array([10, 255, 140])
-l_b_2 = np.array([170, 200, 94])
-u_b_2 = np.array([255, 255, 140])
+l_b_1 = np.array([0, 200, 80])
+u_b_1 = np.array([10, 255, 160])
+l_b_2 = np.array([170, 200, 80])
+u_b_2 = np.array([255, 255, 160])
 
 def do_blob_detection(mask):
     params = cv2.SimpleBlobDetector_Params()
@@ -19,19 +19,19 @@ def do_blob_detection(mask):
 
     # Filter by Area.
     params.filterByArea = True
-    params.minArea = 30
-    params.maxArea = 20000
+    params.minArea = 10
+    params.maxArea = 200000000
 
     # Filter by Circularity
-    params.filterByCircularity = True
+    params.filterByCircularity = False
     params.minCircularity = 0.1
 
     # Filter by Convexity
-    params.filterByConvexity = True
+    params.filterByConvexity = False
     params.minConvexity = 0.5
 
     # Filter by Inertia
-    params.filterByInertia =True
+    params.filterByInertia =False
     params.minInertiaRatio = 0.5
 
     detector = cv2.SimpleBlobDetector_create(params)
@@ -41,11 +41,10 @@ def do_blob_detection(mask):
 
 def get_relative_pos(image, keypoints):
     points2f =	cv2.KeyPoint_convert(keypoints)
-    print(points2f)
 
     rows = float(image.shape[0])
     cols = float(image.shape[1])
-    print(rows, cols)
+    # print(cols, rows)
 
     center_x    = 0.5*cols
     center_y    = 0.5*rows
@@ -71,21 +70,26 @@ def callback(data):
 
     #do blob detection and draw around ball in image
     keypoints =  do_blob_detection(rev_mask)
+    if keypoints:
+
+        #publish ball point relative to center negative for left, positive for right - scaled to one
+        x,y = get_relative_pos(cv_image,keypoints)
+        print("publishing: ",x,y)
+        blob_msg = Point()
+        blob_msg.x = x
+        blob_msg.y = y
+        image_pub.publish(blob_msg)
+
+    #mask and image with keypoints
     im_with_keypoints = cv2.drawKeypoints(cv_image, keypoints, np.array([]), (0,255,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    cv2.imshow("mask", rev_mask)
+    cv2.imshow("blob", im_with_keypoints)
+    cv2.waitKey(2)
 
-    # cv2.imshow("blob", im_with_keypoints)
-    # cv2.waitKey(0)
-
-    #publish ball point relative to center
-    x,y = get_relative_pos(cv_image,keypoints)
-    blob_msg = Point()
-    blob_msg.x = x
-    blob_msg.y = y
-    image_pub.publish(blob_msg)
 
 if __name__ == '__main__':
     rospy.init_node('ball_detection', anonymous=True)
-    image_pub = rospy.Publisher("/turtlebot3_waffle_pi/ball_pos",Point,queue_size=1)
-    image_sub = rospy.Subscriber("/turtlebot3_waffle_pi/camera1/image_raw", Image, callback)
+    image_pub = rospy.Publisher("/tb3_0/rel_ball_loc/",Point,queue_size=1)
+    image_sub = rospy.Subscriber("/tb3_0/camera/rgb/image_raw", Image, callback)
     rospy.spin()
     cv2.destroyAllWindows()
